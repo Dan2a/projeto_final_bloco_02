971255447fe75cd2bc644a15964728c758e9df96
Daniel Andrade <dan.andrade313@gmail.com>
2025-12-16 11:09:38 -0300

    criado service de produto

--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Produto } from "./entities/produto.entity";
-import { CategoriaModule } from "../categoria/categoria.module";
+import { ProdutoService } from "./services/produto.service";
+import { Categoria } from "../categoria/entities/categoria.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Produto]), CategoriaModule],
-    providers: [],
+    imports: [TypeOrmModule.forFeature([Produto, Categoria])],
+    providers: [ProdutoService],
     controllers: [],
     exports: [],
 })
---
+++ src/produto/services/produto.service.ts
@@ -0,0 +1,75 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
+import { Produto } from "../entities/produto.entity";
+import { Categoria } from "../../categoria/entities/categoria.entity";
+
+@Injectable()
+export class ProdutoService {
+    constructor(
+        @InjectRepository(Produto)
+        private produtoRepository: Repository<Produto>,
+
+        @InjectRepository(Categoria)
+        private categoriaRepository: Repository<Categoria>,
+    ) { }
+
+    async findAll(): Promise<Produto[]> {
+        return await this.produtoRepository.find({
+            relations: ['categoria'],
+        });
+    }
+
+    async findById(id: number): Promise<Produto> {
+        let buscaProduto = await this.produtoRepository.findOne({
+            where: { id },
+            relations: ['categoria']
+        })
+
+        if (!buscaProduto) {
+            throw new HttpException('Produto não encontrado', HttpStatus.NOT_FOUND);
+        }
+
+        return buscaProduto;
+    }
+
+    async findByName(nome: string): Promise<Produto[]> {
+        return this.produtoRepository.find({
+            where: { nome: ILike(`%${nome}%`) },
+            relations: ['categoria'],
+        });
+    }
+
+    async create(produto: Produto): Promise<Produto> {
+        await this.validarCategoria(produto.categoria);
+
+        return await this.produtoRepository.save(produto);
+    }
+
+    async update(produto: Produto): Promise<Produto> {
+        await this.findById(produto.id);
+
+        await this.validarCategoria(produto.categoria);
+
+        return await this.produtoRepository.save(produto);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        await this.findById(id);
+
+        return await this.produtoRepository.delete(id);
+    }
+
+    private async validarCategoria(categoria: Categoria): Promise<void> {
+        if (categoria) {
+            let buscaCategoria = await this.categoriaRepository.findOne({
+                where: { id: categoria.id }
+            });
+
+            if (!buscaCategoria) {
+                throw new HttpException('Categoria não encontrada!', HttpStatus.NOT_FOUND);
+            }
+        }
+    }
+
+}
